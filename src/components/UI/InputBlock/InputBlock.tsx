b50072dbc7d2a39a75d85140a29e6061de3e0c02
sasha <sasha.v.bes@gmail.com>
2023-07-18 00:17:34 +0300

    create StepThree

--- src/components/UI/InputBlock/InputBlock.tsx
+++ src/components/UI/InputBlock/InputBlock.tsx
@@ -3,8 +3,11 @@ import { TextField } from "@mui/material";
 import { FC } from "react";
 import { RegisterOptions, UseFormRegister } from "react-hook-form";
 import { AnimatePresence, motion } from "framer-motion";
+import cn from "classnames";
 
 interface IInputBlock {
+  // sizeStyle?: { unit: "кг" | "см"  };
+  sizeStyle?: "кг" | "см";
   name: string;
   label?: string;
   size: "small" | "medium";
@@ -22,28 +25,36 @@ export const InputBlock: FC<IInputBlock> = ({
   size,
   type,
   label,
+  sizeStyle,
 }) => {
   return (
-    <div className={styles.main}>
-      <TextField
-        type={type}
-        size={size}
-        className={styles.inp}
-        label={label}
-        {...(register && register(name, { ...rules }))}
-      />
+    <div className={styles.sizingBlock}>
+      <div
+        className={cn(styles.main, {
+          [styles.sizeStyle]: sizeStyle === "кг" || sizeStyle === "см",
+        })}
+      >
+        <TextField
+          type={type}
+          size={size}
+          className={styles.inp}
+          label={label}
+          {...(register && register(name, { ...rules }))}
+        />
 
-      <AnimatePresence>
-        {errors[name] && (
-          <motion.p
-            initial={{ height: 0, y: "-100" }}
-            animate={{ height: "auto", y: 0 }}
-            exit={{ height: 0, y: "-100" }}
-          >
-            {errors[name].message}
-          </motion.p>
-        )}
-      </AnimatePresence>
+        <AnimatePresence>
+          {errors[name] && (
+            <motion.p
+              initial={{ height: 0, y: "-100" }}
+              animate={{ height: "auto", y: 0 }}
+              exit={{ height: 0, y: "-100" }}
+            >
+              {errors[name].message}
+            </motion.p>
+          )}
+        </AnimatePresence>
+      </div>
+      {sizeStyle && <p className={styles.sizingBlock__unit}>{sizeStyle}</p>}
     </div>
   );
 };
--- src/pages/StepThree/StepThree.tsx
+++ src/pages/StepThree/StepThree.tsx
@@ -1,16 +1,17 @@
+import styles from "./StepThree.module.scss";
 import { Button } from "@mui/material";
 import { Container } from "../../components/Container/Container";
 import { Navigation } from "../../components/Navigation/Navigation";
-import styles from "./StepThree.module.scss";
 import { SubmitErrorHandler, SubmitHandler, useForm } from "react-hook-form";
 import { IStepThree } from "../../types/StepsInterfaces";
+import { InputBlock } from "../../components/UI/InputBlock/InputBlock";
 
 export const StepThree = () => {
   const {
     handleSubmit,
     register,
-    formState: { isValid, submitCount },
-  } = useForm<IStepThree>();
+    formState: { isValid, submitCount , errors},
+  } = useForm<IStepThree>({ mode: "onBlur", reValidateMode: "onChange" });
 
   const error: SubmitErrorHandler<IStepThree> = (data) => {
     console.log("error", data);
@@ -25,11 +26,55 @@ export const StepThree = () => {
       <form className={styles.formBlock} onSubmit={handleSubmit(submit, error)}>
         <Navigation />
 
-        <div className="">
-
-
-
-          
+        <div className={styles.inputsBlock}>
+          <InputBlock
+            errors={errors}
+            name="weight"
+            register={register}
+            rules={{
+              required: { value: true, message: "required" },
+              max: { value: 30, message: "максимум 30 кг" },
+            }}
+            size="small"
+            type="number"
+            sizeStyle={"кг"}
+          />
+          <InputBlock
+            errors={errors}
+            name="width"
+            register={register}
+            rules={{
+              required: { value: true, message: "required" },
+              max: { value: 100, message: "максимум 100см" },
+            }}
+            size="small"
+            type="number"
+            sizeStyle={"см"}
+          />
+          <InputBlock
+            errors={errors}
+            name="height"
+            register={register}
+            rules={{
+              required: { value: true, message: "required" },
+              max: { value: 100, message: "максимум 100см" },
+            }}
+            size="small"
+            type="number"
+            sizeStyle={"см"}
+          />
+          <InputBlock
+            errors={errors}
+            name="lenth"
+            register={register}
+            rules={{
+              required: { value: true, message: "required" },
+              max: { value: 100, message: "максимум 100см" },
+            }}
+            size="small"
+            type="number"
+            sizeStyle={"см"}
+          />
         </div>
 
         <Button

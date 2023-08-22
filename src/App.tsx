6203cffad88d1f04593b39da7b5a7f7bacf3c3b2
sasha <sasha.v.bes@gmail.com>
2023-08-22 14:29:13 +0300

    f

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { Header } from "./components/Header/Header";
-import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { StepTwo } from "./pages/StepTwo/StepTwo";
 import { StepOne } from "./pages/StepOne/StepOne";
 import { Layout } from "./pages/Layout/Layout";
@@ -20,42 +19,43 @@ function App() {
       <OrderProvider>
         <DataProvider>
 
+          <Routes>
 
-          <HashRouter basename="/">
-            <Routes>
-              <Route path="/" element={<Layout />}>
+            <Route path="/" element={<Layout />}>
 
-                <Route index element={<StepOne />} />
+              <Route index element={<StepOne />} />
 
-                <Route path="step-two" element={
-                  <RequireAccess>
-                    <StepTwo />
-                  </RequireAccess>}
-                />
+              <Route path="step-two" element={
+                <RequireAccess>
+                  <StepTwo />
+                </RequireAccess>}
+              />
 
-                <Route path="step-three" element={
-                  <RequireAccess>
-                    <StepThree />
-                  </RequireAccess>}
-                />
+              <Route path="step-three" element={
+                <RequireAccess>
+                  <StepThree />
+                </RequireAccess>}
+              />
 
-                <Route path="step-four" element={
-                  <RequireAccess>
-                    <StepFour />
-                  </RequireAccess>}
-                />
+              <Route path="step-four" element={
+                <RequireAccess>
+                  <StepFour />
+                </RequireAccess>}
+              />
 
-                <Route path="step-five" element={
-                  <RequireAccess>
-                    <StepFive />
-                  </RequireAccess>}
-                />
+              <Route path="step-five" element={
+                <RequireAccess>
+                  <StepFive />
+                </RequireAccess>}
+              />
 
-                <Route path="orders" element={<Packages />} />
-                <Route path="*" element={<NotFoundPages />} />
-              </Route>
-            </Routes>
-          </HashRouter>
+              <Route path="orders" element={<Packages />} />
+
+              <Route path="*" element={<NotFoundPages />} />
+
+            </Route>
+            
+          </Routes>
 
         </DataProvider>
       </OrderProvider>
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { DataProvider } from './providers/DataContext';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
-import { OrderProvider } from './providers/OrdersContext';
-import AccessProvider from './providers/AccessProvider';
+import { HashRouter } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -14,15 +12,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
 
-    {/* <AccessProvider>
-      <OrderProvider>
-        <DataProvider> */}
-
-    <App />
-
-    {/* </DataProvider>
-      </OrderProvider>
-    </AccessProvider> */}
+    <HashRouter basename="/">
+      <App />
+    </HashRouter>
 
   </React.StrictMode>
 );
